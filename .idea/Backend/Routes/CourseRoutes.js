7dc9fdc920777861798413aa2175d75744300aa0
nedara1 <nedara1@umbc.edu>
2025-05-09 00:17:24 -0400

    Final_Demo_Commit

--- .idea/Backend/Routes/CourseRoutes.js
+++
@@ -1,9 +0,0 @@
-// Routes/CourseRoutes.js
-const express = require('express');
-const router  = express.Router();
-const { getCourseById } = require('../Controllers/CourseController');
-
-// GET /api/course/:courseId
-router.get('/:courseId', getCourseById);
-
-module.exports = router;
